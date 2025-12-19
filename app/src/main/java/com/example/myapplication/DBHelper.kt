4ff5079bcef418bd518a9d4e6074d723cc35e2c0
PhongTran0808 <phongtran080809@gmail.com>
2025-12-19 14:26:46 +0700

    first commit

--- app/src/main/java/com/example/myapplication/DBHelper.kt
+++ app/src/main/java/com/example/myapplication/DBHelper.kt
@@ -6,7 +6,7 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DBHelper(context: Context?) : SQLiteOpenHelper(context, DBNAME, null, 1) {
+class DBHelper(context: Context?) : SQLiteOpenHelper(context, DBNAME, null, 2) {
 
     companion object {
         const val DBNAME = "Login.db"
@@ -14,12 +14,13 @@ class DBHelper(context: Context?) : SQLiteOpenHelper(context, DBNAME, null, 1) {
 
     override fun onCreate(MyDB: SQLiteDatabase) {
         MyDB.execSQL("create Table users(username TEXT primary key, password TEXT)")
-        MyDB.execSQL("create Table tasks(id INTEGER primary key autoincrement, task TEXT, username TEXT)")
+        MyDB.execSQL("create Table items(id INTEGER primary key autoincrement, name TEXT, username TEXT)")
     }
 
     override fun onUpgrade(MyDB: SQLiteDatabase, i: Int, i1: Int) {
         MyDB.execSQL("drop Table if exists users")
-        MyDB.execSQL("drop Table if exists tasks")
+        MyDB.execSQL("drop Table if exists items")
+        onCreate(MyDB)
     }
 
     fun insertData(username: String, password: String): Boolean {
@@ -43,31 +44,31 @@ class DBHelper(context: Context?) : SQLiteOpenHelper(context, DBNAME, null, 1) {
         return cursor.count > 0
     }
 
-    fun insertTask(task: String, username: String): Boolean {
+    fun insertItem(name: String, username: String): Boolean {
         val MyDB = this.writableDatabase
         val contentValues = ContentValues()
-        contentValues.put("task", task)
+        contentValues.put("name", name)
         contentValues.put("username", username)
-        val result = MyDB.insert("tasks", null, contentValues)
+        val result = MyDB.insert("items", null, contentValues)
         return result != -1L
     }
 
-    fun getTasks(username: String): Cursor? {
+    fun getItems(username: String): Cursor? {
         val MyDB = this.readableDatabase
-        return MyDB.rawQuery("SELECT id as _id, task FROM tasks WHERE username = ?", arrayOf(username))
+        return MyDB.rawQuery("SELECT id as _id, name FROM items WHERE username = ?", arrayOf(username))
     }
 
-    fun updateTask(id: String, task: String): Boolean {
+    fun updateItem(id: String, name: String): Boolean {
         val MyDB = this.writableDatabase
         val contentValues = ContentValues()
-        contentValues.put("task", task)
-        val result = MyDB.update("tasks", contentValues, "id = ?", arrayOf(id))
+        contentValues.put("name", name)
+        val result = MyDB.update("items", contentValues, "id = ?", arrayOf(id))
         return result > 0
     }
 
-    fun deleteTask(id: String): Boolean {
+    fun deleteItem(id: String): Boolean {
         val MyDB = this.writableDatabase
-        val result = MyDB.delete("tasks", "id = ?", arrayOf(id))
+        val result = MyDB.delete("items", "id = ?", arrayOf(id))
         return result > 0
     }
 }
